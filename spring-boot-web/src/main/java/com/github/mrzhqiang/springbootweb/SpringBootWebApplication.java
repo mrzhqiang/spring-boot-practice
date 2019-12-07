1e4a0f19fc0327145fdf2c3cd003cfe4f14e4f9c
mrzhqiang <287431404@qq.com>
2019-12-07 15:58:24 +0800

    更新：7.5 Tomcat 配置

--- spring-boot-web/src/main/java/com/github/mrzhqiang/springbootweb/SpringBootWebApplication.java
+++ spring-boot-web/src/main/java/com/github/mrzhqiang/springbootweb/SpringBootWebApplication.java
@@ -1,8 +1,14 @@
 package com.github.mrzhqiang.springbootweb;
 
 import java.util.ArrayList;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,5 +31,30 @@ public class SpringBootWebApplication {
     model.addAttribute("singlePerson", single);
     model.addAttribute("people", people);
     return "index";
+  }
+
+  @Bean
+  public TomcatServletWebServerFactory serverContainer() {
+    TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory() {
+      @Override protected void postProcessContext(Context context) {
+        SecurityConstraint securityConstraint = new SecurityConstraint();
+        securityConstraint.setUserConstraint("CONFIDENTIAL");
+        SecurityCollection securityCollection = new SecurityCollection();
+        securityCollection.addPattern("/*");
+        securityConstraint.addCollection(securityCollection);
+        context.addConstraint(securityConstraint);
+      }
+    };
+    serverFactory.addAdditionalTomcatConnectors(initiateHttpConnector());
+    return serverFactory;
+  }
+
+  private Connector initiateHttpConnector() {
+    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+    connector.setScheme("http");
+    connector.setPort(8080);
+    connector.setSecure(false);
+    connector.setRedirectPort(8443);
+    return connector;
   }
 }
