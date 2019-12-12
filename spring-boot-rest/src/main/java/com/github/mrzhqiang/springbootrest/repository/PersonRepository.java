d22014bc3bddb5feb0d046f4fee335fcce6b0c41
mrzhqiang <287431404@qq.com>
2019-12-12 20:57:07 +0800

    更新：8.3 Spring Data Rest

---
+++ spring-boot-rest/src/main/java/com/github/mrzhqiang/springbootrest/repository/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.github.mrzhqiang.springbootrest.repository;
+
+import com.github.mrzhqiang.springbootrest.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+@RepositoryRestResource(path = "people")
+public interface PersonRepository extends JpaRepository<Person, Long> {
+  @RestResource(path = "nameStartsWith", rel = "nameStartsWith")
+  Person findByNameStartsWith(@Param("name") String name);
+}
---
+++ spring-boot-rest/src/test/java/com/github/mrzhqiang/springbootrest/SpringBootRestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.mrzhqiang.springbootrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
