c78d2b99b151631499f8f94697f58b6dcab10c0d
mrzhqiang <287431404@qq.com>
2019-12-24 22:53:39 +0800

    更新：测试数据可以回滚

--- spring-boot-rest/src/test/java/com/github/mrzhqiang/springbootrest/SpringBootRestApplicationTests.java
+++ spring-boot-rest/src/test/java/com/github/mrzhqiang/springbootrest/SpringBootRestApplicationTests.java
@@ -14,9 +14,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.context.WebApplicationContext;
 
 @SpringBootTest
+@Transactional
 class SpringBootRestApplicationTests {
 
   @Autowired PersonRepository personRepository;
