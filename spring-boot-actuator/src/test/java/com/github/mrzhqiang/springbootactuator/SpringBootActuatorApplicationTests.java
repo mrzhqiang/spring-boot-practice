c1fc5deb3f7be868026bb47f5b7d0c48d35736ad
mrzhqiang <287431404@qq.com>
2019-12-27 11:02:37 +0800

    更新：11 应用监控

---
+++ spring-boot-actuator/src/test/java/com/github/mrzhqiang/springbootactuator/SpringBootActuatorApplicationTests.java
@@ -0,0 +1,12 @@
+package com.github.mrzhqiang.springbootactuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
